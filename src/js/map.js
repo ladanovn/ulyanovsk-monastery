831b349f8377ca0bcaa9b918f8f80a7aebd6c6cb
Ladanov Nikolay <ladanov_n@mail.ru>
2019-04-23 00:37:26 +0300

    fixed font size

--- src/js/map.js
+++ src/js/map.js
@@ -207,5 +207,5 @@ function resize(view) {
   view.style.height = `${cover_height}px`;
   view.style.top = `${(window_height - cover_height) / 2}px`;
   view.style.left = `${(window_width - cover_width) / 2}px`;
-  view.style.fontSize = `${(cover_width / 1920) * 18}px`;
+  document.body.style.fontSize = `${(cover_width / 1920) * 18}px`;
 }
